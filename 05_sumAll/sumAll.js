09cea9d6614dede38c72d7e2c6cf7548adbae940
Ntt37Vp <fuckspammers@protonmail.com>
2023-11-19 16:03:44 +0800

    exercises 5, 6 & 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    let sum = 0;
+    for (let i = intA; i <= intB; i++ ) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
